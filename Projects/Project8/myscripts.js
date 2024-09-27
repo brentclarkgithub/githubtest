0b3e0e5a11aecb798262c36f9b7b3fac22c5acd1
brentclarkgithub <brent.clark@ucas-edu.net>
2024-09-27 12:06:09 -0600

    made some changes

---
+++ Projects/Project8/myscripts.js
@@ -0,0 +1,54 @@
+﻿	PrintButton1 = new Image
+	PrintButton2 = new Image
+	logo1 = new Image
+	logo2 = new Image
+if (document.images) {
+	PrintButton1.src = "images/Print1.jpg"
+	PrintButton2.src = "images/Print2.jpg"
+	logo1.src = "images/logo1.jpg"
+	logo2.src = "images/logo2.jpg"
+}
+
+
+		var imgArray = new Array(
+			'Image1Lg.jpg',
+			'Image2Lg.jpg',
+			'Image3Lg.jpg',
+			'Image4Lg.jpg'
+			
+
+		);
+
+
+		var titleArray = new Array(
+			'Hunting Couple',
+			'Biking Couple',
+			'Scuba Couple',
+			'Skiing Couple'
+			
+		);
+			
+		var imgPath = "Images/Fullsize/";
+		
+		function swapImage(imgID) {
+
+			var theImage = document.getElementById('theImage');
+			var textDiv = document.getElementById('bottomText');
+
+			var newImg;
+			var textTitle;
+
+			newImg = imgArray[imgID];
+			theImage.src = imgPath + newImg;
+
+			textTitle=titleArray[imgID];
+
+      			textDiv.innerHTML = textTitle;
+		}
+			
+		function preloadImages() {		
+			for(var i = 0; i < imgArray.length; i++) {
+				var tmpImg = new Image;
+				tmpImg.src = imgPath + imgArray[i];
+			}
+		}
